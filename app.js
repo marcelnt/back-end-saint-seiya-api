ac2d00592c21b2c7fdceeb68eeaf55a58fe41712
MARCEL NEVES TEIXEIRA <sn50069@CFP127ALUNO.LOCAL>
2025-06-09 14:59:19 -0300

    Back Saint Seiya

---
+++ app.js
@@ -0,0 +1,81 @@
+/******************************************************************************************
+ * Objetivo: API para manipular dados SAINT SEIYA
+ * Data: 09/06/2025
+ * Autor: Marcel
+ * Versão: 1.0
+ ******************************************************************************************/
+
+/*
+    Para criar uma API devemos instalar:
+        express         npm install express --save          - Serve para criar a API
+        cors            npm install cors --save             - Serve para configurar as permissões da API
+        body-parser     npm install body-parser --save      - Serve para manipular os dados do body da API
+*/
+
+const PORT = process.env.PORT || 8080; 
+
+//Import das bibliotecas para criar uma API
+const express       = require('express')
+const cors          = require('cors')
+const bodyParser    = require('body-parser')
+
+//Inicializando o express através do objeto app
+const app = express()
+
+app.use((request, response, next)=>{
+    //Permissão de acesso para liberar quais computadores poderão acessar a API
+    response.header('Access-Control-Allow-Origin', '*')
+    //Permissão de acesso para liberar os verbos da requisição da API
+    response.header('Access-Control-Allow-Methods', 'GET')
+
+    app.use(cors())//Ativando as configurações no cors
+
+    next()
+})
+
+//Import do arquivo de funções
+const dados = require('./modulo/funcoes.js')
+
+//EndPoint para retornar as siglas dos estados
+app.get('/v1/saint-seiya/characters', cors(), async function (request, response){
+
+    //Chama a função que vai retornar a lista dos estados
+    let dadosCharacters = dados.getAllCharacters()
+
+    if(dadosCharacters){
+        response.status(dadosCharacters.status_code) //Sucess
+        response.json(dadosCharacters)
+    }
+})
+
+//EndPoint para retornar as caracteristicas de um estado filtrando pela sigla
+app.get('/v1/saint-seiya/characters/rank', cors(), async function(request, response){
+    //Recebe o parametro(variavel) :sigla pela URL
+    let rank = request.query.search
+ 
+    //Chama a função que vai retornar a lista dos estados
+    let dadosCharacters = dados.getCharactersRank(rank)
+
+    if(dadosCharacters){
+        response.status(dadosCharacters.status_code) //Sucess
+        response.json(dadosCharacters)
+    }
+})
+
+//EndPoint para retornar as caracteristicas de um estado filtrando pela sigla
+app.get('/v1/saint-seiya/characters/:id', cors(), async function(request, response){
+    //Recebe o parametro(variavel) :sigla pela URL
+    let id = request.params.id
+ 
+    //Chama a função que vai retornar a lista dos estados
+    let dadosCharacters = dados.getCharactersById(id)
+
+    if(dadosCharacters){
+        response.status(dadosCharacters.status_code) //Sucess
+        response.json(dadosCharacters)
+    }
+})
+
+app.listen(PORT, function(){
+    console.log('API aguardando requisições ...')
+})
---
+++ modulo/funcoes.js
@@ -0,0 +1,98 @@
+/* *********************************************************************
+* Objetivo: Obter uma lista de Personagens Saint Seiya
+* Data: 09/06/2025
+* Autor: Marcel
+* Versão: 1.0
+* **********************************************************************/
+
+const listImportDados       = require('./saint_seya.js')
+const message_error_not_found = {development: 'Marcel Neves Teixeira', status: 'true', status_code: 404, message: 'Nenhum item encontrado!!!'}
+
+//Retorna todos os personagens
+const getAllCharacters = function(){
+
+    let listDadosJSON = {};
+
+    let status = false
+
+    if(listImportDados.dados.data.characters.length > 0){
+        listDadosJSON.development = 'Marcel Neves Teixeira'
+        listDadosJSON.status = true
+        listDadosJSON.status_code = 200
+        listDadosJSON.length = listImportDados.dados.data.characters.length
+        listDadosJSON.characters = listImportDados.dados.data.characters
+        status = true
+    }
+
+    if (!status)
+        return message_error_not_found
+    else
+        return listDadosJSON
+};
+
+//Retorna personagens pelo tipo
+const getCharactersRank = function(rank){
+    let arrayListCharacter    = [] 
+    let typeCharacter = String(rank).toUpperCase()
+    let listDadosJSON = {};
+    let status = false
+
+    listImportDados.dados.data.characters.forEach(function(item){
+
+        console.log(String(item.rank.toUpperCase()))
+        console.log(typeCharacter)
+        if(String(item.rank.toUpperCase()) == typeCharacter){
+            arrayListCharacter.push(item)
+            status = true
+        }
+    })
+
+    if(status){
+        listDadosJSON.development = 'Marcel Neves Teixeira'
+        listDadosJSON.status = true
+        listDadosJSON.status_code = 200
+        listDadosJSON.length = arrayListCharacter.length
+        listDadosJSON.characters = arrayListCharacter
+    }
+
+    console.log(arrayListCharacter)
+    if (!status)
+        return message_error_not_found
+    else
+        return listDadosJSON
+};
+
+//Retorna personagens pelo ID
+const getCharactersById = function(id){
+    let arrayListCharacter    = [] 
+    let listDadosJSON = {};
+    let status = false
+
+    listImportDados.dados.data.characters.forEach(function(item){
+        if(String(item.id) == id){
+            arrayListCharacter.push(item)
+            status = true
+        }
+    })
+
+    if(status){
+        listDadosJSON.development = 'Marcel Neves Teixeira'
+        listDadosJSON.status = true
+        listDadosJSON.status_code = 200
+        listDadosJSON.length = arrayListCharacter.length
+        listDadosJSON.characters = arrayListCharacter
+    }
+
+    console.log(arrayListCharacter)
+    if (!status)
+        return message_error_not_found
+    else
+        return listDadosJSON
+};
+
+
+module.exports = {
+    getAllCharacters,
+    getCharactersRank,
+    getCharactersById
+}
